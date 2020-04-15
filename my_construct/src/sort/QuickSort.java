f2b30231ea453f55289f4023c4714df7e7812dfc
liuzt <liuzt@tranzvision.com.cn>
2020-04-15 10:55:05 +0800

    test

--- my_construct/src/sort/QuickSort.java
+++ my_construct/src/sort/QuickSort.java
@@ -1,13 +1,25 @@
 package sort;
 
+import java.util.LinkedList;
+
 /**
  * 快速排序
  */
 public class QuickSort {
     public static void main(String[] args) {
 
+
+
     }
-    public static Integer[] quickSort(Integer[] arr){
+    public static Integer[] quickSort(Integer[] arr,int start,int end){
+
+        if (end - start > 1){
+            Integer tem = arr[start];
+
+
+        }
+
+
         return null;
     }
 }
