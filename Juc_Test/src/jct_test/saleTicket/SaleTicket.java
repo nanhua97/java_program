dc0ae70ea100ed7909cd56df48da3d1a324e97c6
liuzt <liuzt@tranzvision.com.cn>
2020-07-27 17:14:47 +0800

    Juc -- SaleTicket

---
+++ Juc_Test/src/jct_test/saleTicket/SaleTicket.java
@@ -0,0 +1,51 @@
+package jct_test.saleTicket;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 线程       操作      资源类
+ */
+public class SaleTicket {
+    public static void main(String[] args) {
+        Ticket ticket = new Ticket();
+        new Thread(()->{
+            for (int i = 0; i < 30; i++) {
+                ticket.saleTicket();
+            }
+        }).start();
+        new Thread(()->{
+            for (int i = 0; i < 30; i++) {
+                ticket.saleTicket();
+            }
+        }).start();
+        new Thread(()->{
+            for (int i = 0; i < 30; i++) {
+                ticket.saleTicket();
+            }
+        }).start();
+    }
+}
+
+/**
+ * 资源类 = 实例变量 + 实例方法
+ */
+class Ticket{
+    private Integer tickets = 30;
+    Lock lock = new ReentrantLock();
+
+    public void saleTicket(){
+        lock.lock();
+        try {
+            if (tickets>0){
+                System.out.println(Thread.currentThread().getName()+"卖出第"+tickets--+"张票");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            lock.unlock();
+        }
+
+
+    }
+}
